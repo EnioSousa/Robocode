cb56be91572e3afbeafa88f9530cdf329b8dea2b
enio95 <eniofcup@hotmail.com>
2021-02-16 18:45:09 +0000

    Basic move, scan mechanics

--- src/robot/FirstRobot.java
+++ src/robot/FirstRobot.java
@@ -1,98 +1,181 @@
 package robot;
 import robocode.*;
 import robocode.util.Utils;
+import java.util.Random;
 
-public class FirstRobot extends AdvancedRobot {
-
-	boolean focus = false;
-	double extraPower = 0;
-	int counter = 0;
+public class FirstRobot extends AdvancedRobot 
+{
+	/* Random generator*/
+	Random gen = new Random();
 	
+	/* Tick counter*/
+	int tick = 0;
+	
+	/* Keeps track of enemy. If null no enemy scanned*/
+	String enemy = null; 
+	/* Keeps track how many ticks we have been looking for a anemy*/
+	int lookTime = 0;
+	/* Specifies how much to turn the radar*/
+	double radarTurnRate = 10;
+	
+	/*Keeps track of the number of hits*/
+	int hit = 0;
+	
+	/**
+	 * This method will be called by robocode, everything you want the tank to do
+	 * needs to start with this method
+	 */
     public void run() 
     {
+    	/**
+    	 * The following calls enable the tank parts to move independently
+    	 */
     	setAdjustRadarForGunTurn(true);
     	setAdjustRadarForRobotTurn(true);
     	setAdjustGunForRobotTurn(true);
     	
-    	while(true) 
-    	{    		    	
-    		if ( !focus )
-    			setTurnRadarRight(360);
-    		
-    		focus = false;
-    		
-    		execute();
-    	}
-    }
-    
-    public void fireMechanics(double angle, double distance)
-    {
-    	double gunAngle = Utils.normalRelativeAngle(getHeadingRadians() - getGunHeadingRadians() + angle);
-    	
-    	setTurnGunRightRadians(gunAngle);
-    	
-    	System.out.println(gunAngle);
-    	
-    	if ( Math.toDegrees(angle) < 10 )
+    	while (true)
     	{
-    		double power = 1.5;
+    		randomWalk();
+    		scan();
+    		execute();
     		
-    		if ( distance < 100 )
-    			power = 3;
+    		tick++;
     		
-    		else if ( distance < 200 )
-    			power = 2;
-    		
-    		else if ( distance < 300 )
-    			power = 1;
-    		
-    		else
-    			power = 0.5;
-    		
-    		fire ( power );
     	}
+    	
     }
     
-    public void onBulletHit(BulletHitEvent event)
+
+    /*--------------------------Fire mechanics--------------------------*/ 
+    public void robotFire(ScannedRobotEvent event)
     {
-    	System.out.println(counter);
+    	double power = adjustFirePower(event);
     	
-    	if ( counter >= 1 )
-    		extraPower = extraPower >= 2.5 ? 3: extraPower + 0.5;
     	
-    	else
-    		counter++;
-    }
-    
-    public void onBulletMissed(BulletMissedEvent event)
-    {
-    	extraPower = extraPower <= 0.3 ? 0.1: extraPower - 0.2;
     	
-    	counter = 0;
-    }
-    
-    public void onScannedRobot(ScannedRobotEvent e) {
-		// Lock on to our target (this time for sure)
-		setTurnRadarRight(getHeading() - getRadarHeading() + e.getBearing());
-		focus = true;	
-		
-		fireMechanics(e.getBearingRadians(), e.getDistance());
     }
     
     /**
-     * Simple goTo, does not take the best route
-     * @param nx column position
-     * @param ny row position
+     * This method will adjust the fire power to be fired. It takes in consideration 
+     * the distance, previous accuracy and energy levels.
+     * @param event Information about the scanned event
+     * @return the fire power to use
      */
-    public void goTo(double x, double y)
+    public double adjustFirePower(ScannedRobotEvent event)
     {
-    	x -= getX();
-    	y -= getY();
+    	double dist = event.getDistance();
     	
-    	double rotation = Math.atan2(x-getX(), y-getY());
-    	rotation = Utils.normalRelativeAngle(getHeadingRadians() - rotation);
+    	double power = 0;
+    	
+    	/*Criteria: Distance*/    	
+    	power += dist > 400 ? 0.25: dist > 300 ? 0.75: dist > 200 ? 1.25: dist > 100 ? 1.75: 2; 
+
+    	/*Criteria: Previous accuracy*/  
+    	power += hit >= 2 ? 0.5: hit >= 3 ? 1: 1.5;
+
+    	/*Criteria: Current energy*/ 
+    	power -= getEnergy() < 50 ? 1: getEnergy() < 25 ? 1.5: 2;
+    	
+    	return power;    	
+    }
+    
+    /*--------------------------Scan mechanics--------------------------*/   
+    /**
+     * This method should be called in every tick/turn. What it basically does
+     * is to check if we have don't have an enemy in sight, if so then we need to do a 
+     * full scan (full map scan), otherwise we check for number of turns we haven't seen 
+     * our enemy and act appropriately. 
+     */
+    public void scan()
+    {
+    	lookTime++;
+    	
+    	if ( enemy == null )
+    		fullScan();
+    	
+    	/* If we haven't seen our target for x ticks then do something*/
+    	else if ( lookTime > 2 )
+    		setTurnRadarRight(radarTurnRate);
+    	
+    	/* If it reaches here then the radar is at 30+ degrees*/
+    	else if ( lookTime > 5 )
+    		setTurnRadarRight(-radarTurnRate);
+    	
+    	/* If it reaches here the the radar is a -30*/
+    	else if ( lookTime > 11 )
+    	{
+    		fullScan();
+    		enemy = null;
+    	}
+    }
+    
+    /**
+     * Basic full rotation
+     */
+    public void fullScan()
+    {
+		setTurnRadarRight(-2*radarTurnRate);    	
+    }
+
+    /**
+     * The method will be called once we scanned an enemy tank. We calculate the how much
+     * of the radar we have to turn, register the enemy name and reset the lookTime.
+     */
+    public void onScannedRobot(ScannedRobotEvent event)
+    {
+    	lookTime = 0;
+    	enemy = event.getName();
+    	
+    	double rotation = Utils.normalRelativeAngle(getHeadingRadians() - getRadarHeadingRadians() + event.getBearingRadians());
     	
     	setTurnRightRadians(rotation);
-    	setAhead(Math.cos(rotation) * Math.hypot(x, y));
     }
+
+    /*--------------------------Move mechanics--------------------------*/  
+    /**
+     * This method will make the tank walk randomly around the CENTER position.
+     */
+    public void randomWalk()
+    {
+    	if ( tick % 10 != 0 )
+    		return;
+    	
+    	double w = getBattleFieldWidth() / 2 ;
+    	double h = getBattleFieldHeight() / 2;
+    	
+    	/*
+    	 * Remember Random.nextInt generates positive and negative numbers
+    	 */
+    	double offset = gen.nextInt() % 100;
+    	
+    	goTo(w + offset, h + offset);    	
+    }
+    
+    /**
+     * Simple goTo. Robot moves asynchronous to the rest of its components.
+     * This will mean that sometimes the robot will start to move even before the 
+     * correct direction is achieved, sometimes making a curve and being a little 
+     * imprecise.
+     * @param ox column position
+     * @param oy row position
+     */
+    public void goTo(double ox, double oy)
+    {    	
+    	double x = ox - getX();
+    	double y = oy - getY();
+    	
+    	double rotation = Math.atan2(x, y);
+    	rotation = Utils.normalRelativeAngle(rotation - getHeadingRadians());
+    	
+    	setTurnRightRadians(rotation);
+    	double dist = Math.hypot(x, y);
+    	setAhead(dist);
+    	
+    	/**
+    	 * Comment the following line to run test against other robots
+    	 */
+    	System.out.printf("----Movement data----\nRoute set to: (%f, %f)\nDistance: %f\nAngle: %f\nCurrent pos: (%f, %f)\n", ox, oy, dist, Math.toDegrees(rotation), getX(), getY());
+    }
+
 }
